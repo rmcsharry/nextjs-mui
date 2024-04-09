420e9828daea2881a8e42d53855dc76bee433ea3
Richard McSharry <rmcsharry@gmail.com>
2024-04-09 18:20:54 +0200

    create initial frontend structure

---
+++ frontend/pages/about/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const AboutPage = () => {
+  return (
+    <Box>AboutPage</Box>
+  )
+}
+
+export default AboutPage
---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Home Page</Typography>
+    </Box>
+  )
+}
+
+export default HomePage
